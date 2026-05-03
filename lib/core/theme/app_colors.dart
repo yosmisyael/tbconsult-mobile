3f750675792311d1b832ed9f8b0730cdfa9a1ecc
yosev <yosmisyael7308@gmail.com>
2026-05-03 13:04:50 +0700

    feat(app): add color theme

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF006A60);
+  static const Color primaryLight = Color(0xFF03A696);
+  static const Color accentYellow = Color(0xFFE2E83A);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF1A1C1C);
+  static const Color textSecondary = Color(0xFF3D4947);
+}
